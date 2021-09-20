1102f7cf66c3a65e17ca4ce4aed7c826f378e686
LucasSaito <lucas.seiji.saito@usp.br>
2021-09-20 17:47:34 -0300

    removed some files

--- Library/Il2cppBuildCache/Android/armeabi-v7a/il2cppOutput/lumpedcpp/Lump_libil2cpp_mono.cpp
+++
@@ -1,7 +0,0 @@
-#include "il2cpp-config.h"
-#include "/home/lucas/Unity/Hub/Editor/2020.3.18f1/Editor/Data/il2cpp/libil2cpp/mono/MonoPosixHelper.cpp"
-#include "/home/lucas/Unity/Hub/Editor/2020.3.18f1/Editor/Data/il2cpp/libil2cpp/mono/ThreadPool/ThreadPoolMonitorThread.cpp"
-#include "/home/lucas/Unity/Hub/Editor/2020.3.18f1/Editor/Data/il2cpp/libil2cpp/mono/ThreadPool/ThreadPoolWorkerThread.cpp"
-#include "/home/lucas/Unity/Hub/Editor/2020.3.18f1/Editor/Data/il2cpp/libil2cpp/mono/ThreadPool/threadpool-ms-io-poll.cpp"
-#include "/home/lucas/Unity/Hub/Editor/2020.3.18f1/Editor/Data/il2cpp/libil2cpp/mono/ThreadPool/threadpool-ms-io.cpp"
-#include "/home/lucas/Unity/Hub/Editor/2020.3.18f1/Editor/Data/il2cpp/libil2cpp/mono/ThreadPool/threadpool-ms.cpp"
