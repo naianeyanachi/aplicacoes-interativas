47bcabb3a5c06b71c15cca1329bc097122cf6759
Joao Frezzato <jmarcelofo@gmail.com>
2021-12-12 15:59:42 -0300

    Dog Movement

--- Assets/Script/WalkDog.cs
+++ Assets/Script/WalkDog.cs
@@ -1,29 +1,70 @@
 using UnityEngine;
 
-//Attach this script to a GameObject to rotate around the target position.
 public class WalkDog : MonoBehaviour
 {
-  //Assign a GameObject in the Inspector to rotate around
-  public GameObject target;
+  public Transform player;
   public bool walking;
-  Animator animator;
+  private Animator animator;
+
+
+  // Walking variables
+  private float timer;
+  public float timeToMove;
+  public float timeWaiting;
+  public float speed;
+  public float maxX;
+  public float minX;
+  public float maxZ;
+  public float minZ;
+  private Vector3 desiredPos;
 
   void Start()
   {
     animator = GetComponent<Animator>();
+    desiredPos = new Vector3(Random.Range(minX, maxX), 0.0f, Random.Range(minZ, maxZ));
   }
 
   void Update()
   {
-    // Spin the object around the target at 20 degrees/second.
+    timer += Time.deltaTime;
     if (walking)
     {
-      animator.SetInteger("Walk", 1);
-      transform.RotateAround(target.transform.position, Vector3.up, 40 * Time.deltaTime);
+      if (timer >= timeToMove)
+      {
+        animator.SetInteger("Walk", 1);
+        transform.LookAt(desiredPos);
+        transform.position = Vector3.Lerp(transform.position, desiredPos, Time.deltaTime * speed);
+        if (Vector3.Distance(transform.position, desiredPos) <= 0.5f)
+        {
+          desiredPos = new Vector3(Random.Range(minX, maxX), 0.0f, Random.Range(minZ, maxZ));
+          timer = 0.0f;
+          animator.SetInteger("Walk", 0);
+        }
+      }
     }
     else
     {
-      animator.SetInteger("Walk", 0);
+      desiredPos = player.position;
+      desiredPos.y = 0;
+      transform.LookAt(desiredPos);
+      if (Vector3.Distance(transform.position, desiredPos) > 1.6f)
+      {
+        animator.SetInteger("Walk", 1);
+        transform.position = Vector3.Lerp(transform.position, desiredPos, Time.deltaTime * speed);
+        timer = 0.0f;
+      }
+      else
+      {
+        animator.SetInteger("Walk", 0);
+        animator.SetBool("Look", true);
+        if (timer >= timeWaiting)
+        {
+          desiredPos = new Vector3(Random.Range(minX, maxX), 0.0f, Random.Range(minZ, maxZ));
+          timer = 0.0f;
+          walking = true;
+          animator.SetBool("Look", false);
+        }
+      }
     }
   }
   public void FlipWalking()
